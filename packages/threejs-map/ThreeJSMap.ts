f0916386d50e5e9a3c607869f22b7a35867b9b1c
zhangyi <zhangyi@corp-ci.com>
2023-02-02 17:51:06 +0800

    feat: finish line

--- packages/threejs-map/ThreeJSMap.ts
+++ packages/threejs-map/ThreeJSMap.ts
@@ -3,16 +3,27 @@ import {
   Scene,
   PerspectiveCamera,
   WebGLRenderer,
+  BufferGeometry,
+  ShapeGeometry,
   BoxGeometry,
-  MeshBasicMaterial,
   Mesh,
-} from "three"
+  Object3D,
+  Shape,
+  ExtrudeGeometry,
+  MeshBasicMaterial,
+  LineBasicMaterial,
+  Vector3,
+  Line,
+} from "three";
+import * as d3 from 'd3';
+import chinaJSON from '../china-map/china.json'
 
 export default class ThreeJSMap {
   private scene: Scene;
   private camera: PerspectiveCamera;
   private renderer: WebGLRenderer;
   private cube: Mesh;
+  private map: Object3D;
 
   constructor(canvas: HTMLCanvasElement, options: ThreeJSMapOptions) {
     this.scene = new Scene();
@@ -22,13 +33,81 @@ export default class ThreeJSMap {
     });
     renderer.setSize(options.width, options.height);
     this.renderer = renderer;
-    const geometry = new BoxGeometry( 1, 1, 1 );
-    const material = new MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new Mesh( geometry, material );
-    this.scene.add( cube );
-    this.cube = cube;
     this.camera.position.z = 5;
+    this.map = new Object3D();
+    this.initMap();
     this.render();
+  }
+
+  private initMap() {
+    // 魔卡托投影变换
+    const projection = d3
+      .geoMercator()
+      .center([104.0, 37.5])
+      .scale(80)
+      .translate([0, 0])
+    const map = this.map;
+
+    chinaJSON.features.forEach((elem) => {
+      const province = new Object3D();
+
+      // 坐标数组
+      const coordinates = elem.geometry.coordinates;
+      coordinates.forEach(polygons => {
+        polygons.forEach(polygon => {
+          const shape = new Shape();
+          const lineMaterial = new LineBasicMaterial({
+            color: 'white'
+          })
+
+          const points: Vector3[] = [];
+          for (let i = 0; i < polygon.length; i++) {
+            const [x, y] = projection(polygon[i]) as any
+            if (i === 0) {
+              shape.moveTo(x, -y)
+            }
+            shape.lineTo(x, -y);
+            points.push(new Vector3(x, -y, 4.01));
+          }
+
+          const extrudeSettings = {
+            depth: 10,
+            bevelEnabled: false,
+            steps: 2,
+            bevelThickness: 1,
+            bevelSize: 1,
+            bevelOffset: 0,
+            bevelSegments: 1
+          }
+
+          // const geometry = new ExtrudeGeometry(
+          //   shape,
+          //   extrudeSettings
+          // )
+          // const material = new MeshBasicMaterial({
+          //   color: '#2defff',
+          //   transparent: true,
+          //   opacity: 0.6,
+          // })
+          // const material1 = new MeshBasicMaterial({
+          //   color: '#3480C4',
+          //   transparent: true,
+          //   opacity: 0.5,
+          // })
+
+          // const mesh = new Mesh(geometry, [material, material1])
+          const lineGeometry = new BufferGeometry().setFromPoints( points );
+          const line = new Line(lineGeometry, lineMaterial)
+          // province.add(mesh)
+          province.add(line)
+        })
+      })
+
+
+
+      map.add(province);
+      this.scene.add(map);
+    })
   }
 
   private render() {
@@ -36,11 +115,9 @@ export default class ThreeJSMap {
   }
 
   private animate() {
-    requestAnimationFrame(() => {
-      this.animate();
-    });
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    // requestAnimationFrame(() => {
+    //   this.animate();
+    // });
     this.renderer.render(this.scene, this.camera);
   }
 }
