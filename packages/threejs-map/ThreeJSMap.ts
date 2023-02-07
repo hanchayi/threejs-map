4a542a7828e9757ffeec7d185bbf5ab66e0ece0c
zhangyi <zhangyi@corp-ci.com>
2023-02-07 16:01:56 +0800

    feat: raycaster and hover

--- packages/threejs-map/ThreeJSMap.ts
+++ packages/threejs-map/ThreeJSMap.ts
@@ -3,6 +3,7 @@ import {
   Scene,
   PerspectiveCamera,
   WebGLRenderer,
+  Raycaster,
   BufferGeometry,
   Mesh,
   Object3D,
@@ -12,24 +13,38 @@ import {
   LineBasicMaterial,
   Vector3,
   Line,
+  Vector2,
+  Intersection,
+  Material,
 } from "three";
 import * as d3 from 'd3';
 import chinaJSON from '../china-map/china.json'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default class ThreeJSMap {
+  private canvas: HTMLCanvasElement;
   private scene: Scene;
   private camera: PerspectiveCamera;
   private renderer: WebGLRenderer;
   private map: Object3D;
   private control: OrbitControls;
+  private raycaster: Raycaster;
+  private mouse: Vector2;
+  private options: ThreeJSMapOptions;
+  private onMouseMove: (event: MouseEvent) => void;
+  private rect: DOMRect;
+  private lastPick?: Intersection<Mesh>;
 
   constructor(canvas: HTMLCanvasElement, options: ThreeJSMapOptions) {
+    this.canvas = canvas;
+    this.rect = canvas.getBoundingClientRect();
+    this.options = options;
     this.scene = new Scene();
     this.initCamera(options);
     this.initRenderer(canvas, options);
     this.initControl(canvas);
     this.initMap();
+    this.initRaycaster();
     this.render();
   }
 
@@ -50,6 +65,7 @@ export default class ThreeJSMap {
     this.renderer = renderer;
   }
 
+  // 控制器，用于调整相机位置
   private initControl(canvas: HTMLCanvasElement) {
     if (!this.camera || !this.renderer) {
       throw new Error('camera and renderer must init first')
@@ -129,6 +145,23 @@ export default class ThreeJSMap {
     })
   }
 
+  // 射线追踪
+  private initRaycaster() {
+    this.raycaster = new Raycaster();
+    this.mouse = new Vector2();
+    this.onMouseMove = (event) => {
+      // 将鼠标位置归一化为设备坐标。x 和 y 方向的取值范围是 (-1 to +1)
+      this.mouse.x = ((event.clientX - this.rect.left )/ this.options.width) * 2 - 1
+      this.mouse.y = -((event.clientY - this.rect.top) / this.options.height) * 2 + 1
+    }
+
+    this.canvas.addEventListener('mousemove', this.onMouseMove, false)
+  }
+
+  public destroy() {
+    this.canvas.removeEventListener('mousemove', this.onMouseMove)
+  }
+
   private render() {
     this.animate();
   }
@@ -137,6 +170,28 @@ export default class ThreeJSMap {
     requestAnimationFrame(() => {
       this.animate();
     });
+    // 通过摄像机和鼠标位置更新射线
+    this.raycaster.setFromCamera(this.mouse, this.camera)
+    // 算出射线 与当场景相交的对象有那些
+    const intersects = this.raycaster.intersectObjects(
+      this.scene.children,
+      true
+    )
+
+    // 恢复上一次清空的
+    if (this.lastPick) {
+      this.lastPick.object.material[0].color.set('#2defff')
+      this.lastPick.object.material[1].color.set('#3480C4')
+    }
+    this.lastPick = undefined
+    this.lastPick = intersects.find(
+      (item) => item && item.object && (item.object as Mesh).material && ((item.object as Mesh).material as Material[]).length === 2
+    ) as Intersection<Mesh>;
+    if (this.lastPick) {
+      this.lastPick.object.material[0].color.set(0xff0000)
+      this.lastPick.object.material[1].color.set(0xff0000)
+    }
+
     this.renderer.render(this.scene, this.camera);
   }
 }
