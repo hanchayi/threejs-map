f517b995526549ae3beadb297c10c76670b71d27
hanchayi <hanchayi@163.com>
2023-02-13 16:34:52 +0800

    publish

--- packages/map/src/intefaces.ts
+++ packages/map/src/intefaces.ts
@@ -1,7 +1,6 @@
 export interface MapOptions {
   mapUrl: string;
   groundUrl: string;
-  code: string; // 地区码
   debug?: boolean;
   width: number;
   height: number;
@@ -9,9 +8,6 @@ export interface MapOptions {
   cameraY: number; // camera postion y
   cameraZ: number; // camera postion z
   depth: number; // 地图厚度
-  sideColor: string; // 侧边颜色
-  textColor?: string;
-  fillColor?: string;
   adcode: number;
   actives: number[];
   lightIntensity?: number; // light intensity default 1.8
